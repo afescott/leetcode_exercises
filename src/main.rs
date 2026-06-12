dd1ca757dc208130ab98ce5446041a0de6a3c309
afescott <ashley.escott@live.co.uk>
2026-06-12 01:26:29 +0100

    feat: hackerrank making anagrams

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'makeAnagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING a
+ *  2. STRING b
+ */
+
+fn makeAnagram(a: &str, b: &str) -> i32 {
+    let mut hashmap: HashMap<char, i32> = HashMap::new();
+
+    for ele in a.chars() {
+        *hashmap.entry(ele).or_insert(0) += 1;
+    }
+    for ele in b.chars() {
+        *hashmap.entry(ele).or_insert(0) -= 1; // <- entry, not indexing
+    }
+
+    hashmap.values().map(|v| v.abs()).sum()
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -8,22 +31,11 @@ fn main() {
 
     let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
 
-    let mut arr: Vec<Vec<i32>> = Vec::with_capacity(6_usize);
+    let a = stdin_iterator.next().unwrap().unwrap();
 
-    for i in 0..6_usize {
-        arr.push(Vec::with_capacity(6_usize));
+    let b = stdin_iterator.next().unwrap().unwrap();
 
-        arr[i] = stdin_iterator
-            .next()
-            .unwrap()
-            .unwrap()
-            .trim_end()
-            .split(' ')
-            .map(|s| s.to_string().parse::<i32>().unwrap())
-            .collect();
-    }
+    let res = makeAnagram(&a, &b);
 
-    let result = hourglassSum(&arr);
-
-    writeln!(&mut fptr, "{}", result).ok();
+    writeln!(&mut fptr, "{}", res).ok();
 }
