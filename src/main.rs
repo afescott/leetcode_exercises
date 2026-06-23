5a7d323c5485195a13f58a04459072a5e11d24cb
afescott <ashley.escott@live.co.uk>
2026-06-24 00:45:00 +0100

    feat: leetcode add two numbers

--- src/main.rs
+++ src/main.rs
@@ -5,68 +5,106 @@ pub struct ListNode {
     pub next: Option<Box<ListNode>>,
 }
 
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
-
 pub struct Tuple {
     l1: Vec<String>,
-    l1_expired: bool,
+    /*     l1_expired: bool, */
     l2: Vec<String>,
-    l2_expired: bool,
+    /*     l2_expired: bool, */
 }
+
+/* l1 =
+[9]
+l2 =
+[1,9,9,9,9,9,9,9,9,9]
+
+output should be [0,0,0,0,0,0,0,0,0,0,1]
+*/
 
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let mut tuple = Tuple {
-            l1: Vec::new(),
-            l2: Vec::new(),
-            l1_expired: false,
-            l2_expired: false,
-        };
+        let mut dummy = ListNode { val: 0, next: None };
+        let mut tail = &mut dummy;
 
-        let mut curr = l1.as_ref();
-        while let Some(node) = curr {
-            tuple.l1.push(node.val.to_string());
-            curr = node.next.as_ref();
+        let mut carry = 0;
+        // keep going while there's still something to add
+        while l1.is_some() || l2.is_some() || carry > 0 {
+            // get this column's digits (0 if that list is finished)
+            let a = if let Some(ref node) = l1 { node.val } else { 0 };
+            let b = if let Some(ref node) = l2 { node.val } else { 0 };
+            let total = a + b + carry;
+            // write the ones digit, remember if we carry
+            if total >= 10 {
+                tail.next = Some(Box::new(ListNode::new(total - 10)));
+                carry = 1;
+            } else {
+                tail.next = Some(Box::new(ListNode::new(total)));
+                carry = 0;
+            }
+            // move to next node in each list (if any)
+            if l1.is_some() {
+                l1 = l1.unwrap().next;
+            }
+            if l2.is_some() {
+                l2 = l2.unwrap().next;
+            }
+            // move tail to the node we just added
+            tail = tail.next.as_mut().unwrap();
         }
+        dummy.next
 
-        let mut curr = l2.as_ref();
-        while let Some(ref node2) = curr {
-            tuple.l2.push(node2.val.to_string());
-            curr = node2.next.as_ref();
-        }
+        // effort 1
 
-        tuple.l1.reverse();
-        tuple.l2.reverse();
+        /* pub fn add_two_numbers(
+            l1: Option<Box<ListNode>>,
+            l2: Option<Box<ListNode>>,
+        ) -> Option<Box<ListNode>> {
 
-        let str: String = tuple.l1.concat();
 
-        let value_1 = str.parse::<i32>().unwrap_or(0);
+            let mut curr = l1.as_ref();
+            while let Some(node) = curr {
+                tuple.l1.push(node.val.to_string());
+                curr = node.next.as_ref();
+            }
 
-        let str2: String = tuple.l2.concat();
+            let mut curr = l2.as_ref();
+            while let Some(ref node2) = curr {
+                tuple.l2.push(node2.val.to_string());
+                curr = node2.next.as_ref();
+            }
 
-        let value_2 = str2.parse::<i32>().unwrap_or(0);
+            tuple.l1.reverse();
+            tuple.l2.reverse();
 
-        let sum = value_1 + value_2;
+            let str: String = tuple.l1.concat();
 
-        let mut value = None;
-        for ele in sum.to_string().chars() {
-            let digit = ele.to_digit(10).unwrap() as i32;
-            value = Some(Box::new(ListNode {
-                val: digit,
-                next: value,
-            }));
-            println!("ele: {}", ele);
-        }
+            let value_1 = str.parse::<i32>().unwrap_or(0);
 
-        value
+            let str2: String = tuple.l2.concat();
+
+            let value_2 = str2.parse::<i32>().unwrap_or(0);
+
+            let sum = value_1 + value_2;
+
+            println!(
+                "value_1 = {}, value_2 = {}, sum = {}, concatted 1 = {}, concatted 2 = {}",
+                value_1, value_2, sum, str, str2
+            );
+
+            let mut value = None;
+            for ele in sum.to_string().chars() {
+                let digit = ele.to_digit(10).unwrap() as i32;
+                value = Some(Box::new(ListNode {
+                    val: digit,
+                    next: value,
+                }));
+                println!("ele: {}", ele);
+            }
+
+            value
+        } */
     }
 }
 
