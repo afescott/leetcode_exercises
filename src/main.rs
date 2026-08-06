2a71a038a48a17857e51427197f37d9b0d5f09f1
afescott <ashley.escott@live.co.uk>
2026-08-06 16:50:34 +0100

    feat: binary tree  paths final easy

--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,5 @@ impl TreeNode {
         }
     }
 }
-pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
-    let Some(node) = root else { return vec![] };
-    let n = node.borrow();
-    // leaf
-    if n.left.is_none() && n.right.is_none() {
-        return vec![n.val.to_string()];
-    }
-    let mut out = Vec::new();
-    for path in binary_tree_paths(n.left.clone()) {
-        println!("path: {:?}, val: {:?}", path, n.val);
-        out.push(format!("{}->{}", n.val, path));
-    }
-    for path in binary_tree_paths(n.right.clone()) {
-        println!("{:?}", path);
-        out.push(format!("{}->{}", n.val, path));
-    }
-    out
-}
 
 fn main() {}
